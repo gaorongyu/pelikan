186e3931f72be8483457ac5bb835c81f1e156c71
fngry <fngry@163.com>
2018-08-12 15:32:27 +0800

    modify path

--- pelikan-log/src/main/java/com/fngry/pelikan/log/DigestLogger.java
+++ pelikan-log/src/main/java/com/fngry/pelikan/log/DigestLogger.java
@@ -1,6 +1,6 @@
 package com.fngry.pelikan.log;
 
-import com.fngry.monk.common.log.tempalte.TemplateRenderManager;
+import com.fngry.pelikan.log.tempalte.TemplateRenderManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
