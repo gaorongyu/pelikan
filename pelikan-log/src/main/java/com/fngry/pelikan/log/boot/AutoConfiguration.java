6339b86379b5f742a1fde124cd2b4b1d52aa841a
fngry <fngry@163.com>
2018-08-14 22:29:30 +0800

    log auto config

--- pelikan-log/src/main/java/com/fngry/pelikan/log/boot/AutoConfiguration.java
+++ pelikan-log/src/main/java/com/fngry/pelikan/log/boot/AutoConfiguration.java
@@ -1,20 +1,25 @@
 package com.fngry.pelikan.log.boot;
 
-import com.fngry.pelikan.log.LogAutoProxyCreator;
+import com.fngry.pelikan.log.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AutoConfiguration {
 
-//    @Bean
-//    public LogBeanPostProcessor digest() {
-//        return new LogBeanPostProcessor(new DigestInterceptor());
-//    }
+    @Bean
+    public LogBeanPostProcessor digest() {
+        return new LogBeanPostProcessor(new DigestInterceptor());
+    }
 
     @Bean
-    public LogAutoProxyCreator logAutoProxyCreator() {
-        return new LogAutoProxyCreator();
+    public LogBeanPostProcessor logging() {
+        return new LogBeanPostProcessor(new LoggingInterceptor());
     }
+
+//    @Bean
+//    public LogAutoProxyCreator logAutoProxyCreator() {
+//        return new LogAutoProxyCreator();
+//    }
 
 }
